81491560f19ff7d969063cc21790642dc5fe2982
Son Hong Do <sondh0127@gmail.com>
2021-10-01 00:03:44 +0700

    feat: add swift -> js communication

--- ios-hsl/ViewController.swift
+++ ios-hsl/ViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import StreamingKit
+import WebKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var greyView: UIView!
+    
+    @IBOutlet weak var webView: WKWebView!
     
     private let videoPlayer = StreamingVideoPlayer()
     
@@ -23,6 +26,14 @@ class ViewController: UIViewController {
         setupVideoPlayer()
         
         textField.text = "https://dev-livestream.gviet.vn/manifest/VTV1-PACKAGE/master.m3u8"
+                
+        let url = URL(string: "https://dev-livestream.gviet.vn/ilp-statics/v1.3.0/ios-interactive.html")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+    
+
+        let doStuffMessageHandler = "doStuffMessageHandler"
+//        webView.configuration.userContentController.add(self, name: doStuffMessageHandler)
         
     }
     
@@ -54,6 +65,23 @@ class ViewController: UIViewController {
     @IBAction func clearButtonTapped() {
         textField.text = nil
         videoPlayer.pause()
+    }
+    
+    @IBAction func sendMessageToWebview() {
+        let param1 = "bg-blue-600 text-white p-4"
+        let param2 = "bg-red-600 text-white p-4"
+        let data: [String: String] = [
+            "param1": "\(param1)",
+            "param2": "\(param2)"
+        ]
+
+        guard let json = try? JSONEncoder().encode(data),
+              let jsonString = String(data: json, encoding: .utf8) else {
+                return
+         }
+        
+        let javascript = "window.changeButtonClass('\(jsonString)')"
+        webView.evaluateJavaScript(javascript, completionHandler: nil)
     }
 
 
